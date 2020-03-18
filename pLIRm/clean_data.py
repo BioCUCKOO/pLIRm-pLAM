2ae59b1a502c2b8d9d2da81eadd4187ddd871dde
BioCUCKOO <43431339+BioCUCKOO@users.noreply.github.com>
2020-03-18 15:03:06 +0800

    Delete clean_data.py

--- pLIRm/clean_data.py
+++
@@ -1,7 +0,0 @@
-def load_data():
-    p_file = open(r'..\data\data_set.txt', 'r')
-    posi = []
-    for line in p_file.readlines():
-        posi.append(line.strip())
-    p_file.close()
-    return posi
